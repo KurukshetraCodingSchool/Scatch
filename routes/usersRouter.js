ea922a1f73cad6d6ff35b826d3e9ac3325329a6e
kurukshetra <kurukshetracodingschool@gmail.com>
2024-09-04 17:17:22 +0530

    password bcrypt

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const router = express.Router();
+const userModel = require("../models/user-model")
+const bcrypt = require("bcrypt")
 
 router.get("/", function(req,res){
     res.send("hello it's working ");
+})
+router.post("/register",function(req,res){
+   try{
+    let {email , password , fullname } = req.body;
+
+    bcrypt.genSalt(10,function(err, slat){
+      bcrypt.hash(password,slat, async function(err,hash){
+        if (err ) return res.send(err.message);
+        else{
+          let user = await userModel.craete({
+            email,
+            password: hash ,
+            fullname,
+          });
+          res.send(user);
+         }
+        
+      })
+    })
+  }
+    
+   catch(err){
+    console.log(err.message);
+   }
 })
 
 module.exports = router;
